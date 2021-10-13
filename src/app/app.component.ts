0531a8b4478851526381d53329bf672b47899bcc
Christian <christianrey.capoy.20@usjr.edu.ph>
2021-10-13 22:31:35 +0800

    Components_Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ComponentCalculator';
+  title = 'calculator-components';
+  userInput1 = "";
+  userInput2 = "";
+  operationChoice = "";
+
+  // userInput
+  receiveUserInput1($event :any){
+    this.userInput1 = $event;
+
+  }
+  receiveUserInput2($event :any){
+    this.userInput2 = $event;
+
+  }
+
+  // Operation
+  receiveAdd($event : any){
+    this.operationChoice = $event;
+  }
+  receiveSubtract($event : any){
+    this.operationChoice = $event;
+  }
+  receiveMultiply($event : any){
+    this.operationChoice = $event;
+  }
+  receiveDivide($event : any){
+    this.operationChoice = $event;
+  }
+
+
 }
